99cff91aaca730a9270c7982df8c1772afd3d331
Bradley Matola <brmatola@gmail.com>
2022-05-02 16:48:32 -0400

    fix deploy error

--- src/api.tf
+++ src/api.tf
@@ -23,7 +23,7 @@ resource "azurerm_postgresql_server" "server" {
 
   public_network_access_enabled    = true
   ssl_enforcement_enabled          = false
-  ssl_minimal_tls_version_enforced = "TLS1_2"
+  ssl_minimal_tls_version_enforced = "TLSEnforcementDisabled"
 }
 
 resource "azurerm_postgresql_database" "db" {
