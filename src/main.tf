b5ee7ad0b637ab797934009c8b49adc3a05f9e33
Bradley Matola <brmatola@gmail.com>
2022-05-02 13:55:35 -0400

    upgrade to premium to use scoped tokens for auth

--- src/main.tf
+++ src/main.tf
@@ -7,5 +7,5 @@ resource "azurerm_container_registry" "registry" {
   name                = "paylocityChallengeRegistry"
   resource_group_name = azurerm_resource_group.primary.name
   location            = azurerm_resource_group.primary.location
-  sku                 = "Standard"
+  sku                 = "Premium"
 }
