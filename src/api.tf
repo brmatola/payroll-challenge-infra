2214cd915c4682b5b047e0fb16a722c07504e9b5
Bradley Matola <brmatola@gmail.com>
2022-05-02 16:12:55 -0400

    add permissions to pull from acr to web app

--- src/api.tf
+++ src/api.tf
@@ -57,3 +57,8 @@ resource "azurerm_linux_web_app" "api" {
   }
 }
 
+resource "azurerm_role_assignment" "api_pull" {
+  principal_id         = azurerm_linux_web_app.api.identity.0.principal_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.registry.id
+}
