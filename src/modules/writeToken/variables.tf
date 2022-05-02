224c1e9a6cd182a7cce58b7960ea923e8d01bb89
Bradley Matola <brmatola@gmail.com>
2022-05-02 14:51:04 -0400

    get syntax right

---
+++ src/modules/writeToken/variables.tf
@@ -0,0 +1,15 @@
+variable "name" {
+  type = string
+}
+
+variable "repository_name" {
+  type = string
+}
+
+variable "container_registry_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
