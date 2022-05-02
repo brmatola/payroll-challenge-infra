45721089d8c2265d39470b8a17c025e372ce0290
Bradley Matola <brmatola@gmail.com>
2022-05-02 14:16:19 -0400

    we need read permissions as well to push an image

--- src/main.tf
+++ src/main.tf
@@ -14,7 +14,10 @@ resource "azurerm_container_registry_scope_map" "write_api" {
   name                    = "write-api"
   container_registry_name = azurerm_container_registry.registry.name
   resource_group_name     = azurerm_resource_group.primary.name
-  actions                 = ["repositories/api/content/write"]
+  actions = [
+    "repositories/api/content/write",
+    "repositories/api/content/read"
+  ]
 }
 
 resource "azurerm_container_registry_token" "write_api" {
@@ -22,4 +25,5 @@ resource "azurerm_container_registry_token" "write_api" {
   container_registry_name = azurerm_container_registry.registry.name
   resource_group_name     = azurerm_resource_group.primary.name
   scope_map_id            = azurerm_container_registry_scope_map.write_api.id
+  enabled                 = true
 }
