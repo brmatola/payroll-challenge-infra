b60cb9e23da3c27228b23db6e0cea3e846265edb
Bradley Matola <brmatola@gmail.com>
2022-05-02 13:52:23 -0400

    container registry

--- src/main.tf
+++ src/main.tf
@@ -1,4 +1,11 @@
-resource "azurerm_resource_group" "state-demo-secure" {
-  name     = "state-demo"
+resource "azurerm_resource_group" "primary" {
+  name     = "paylocity-challenge"
   location = "eastus"
+}
+
+resource "azurerm_container_registry" "registry" {
+  name                = "paylocityChallengeRegistry"
+  resource_group_name = azurerm_resource_group.primary.name
+  location            = azurerm_resource_group.primary.location
+  sku                 = "Standard"
 }
